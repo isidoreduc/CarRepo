d9b7593736c0d7f7c6b35ff699ef2377abc69109
isidoreduc <isidoreducasse79@gmail.com>
2019-03-14 16:57:35 +0100

    initial setup

---
+++ src/main/java/com/example/cardatabase/CardatabaseApplication.java
@@ -0,0 +1,16 @@
+package com.example.cardatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardatabaseApplication.class, args);
+		System.out.println("Im awesome and I like it");
+	}
+	
+	
+
+}
