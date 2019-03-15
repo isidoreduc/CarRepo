6d4021d879bd721eee26c4e4ab6894d993856c6c
isidoreduc <isidoreducasse79@gmail.com>
2019-03-15 17:04:54 +0100

    bye controller, enters Spring Data Rest

--- src/main/java/com/example/cardatabase/domain/CarRepository.java
+++ src/main/java/com/example/cardatabase/domain/CarRepository.java
@@ -1,7 +1,18 @@
 package com.example.cardatabase.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface CarRepository extends CrudRepository<Car, Long>{
+@RepositoryRestResource
+public interface CarRepository extends CrudRepository<Car, Long> {
+	// can add all sort of GET combinations: findByColorandBrand() etc.
+	
+	// Fetch cars by brand
+	List<Car> findByBrand(@Param("brand") String brand);
 
+	// Fetch cars by color
+	List<Car> findByColor(@Param("color") String color);
 }
--- src/main/java/com/example/cardatabase/web/CarController.java
+++ src/main/java/com/example/cardatabase/web/CarController.java
@@ -1,20 +1,22 @@
 package com.example.cardatabase.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cardatabase.domain.Car;
 import com.example.cardatabase.domain.CarRepository;
 
+/**
+ * 
+ * @author raspi
+ * Controller only needed if you don't use the easier, much
+ * more convenient Spring Data Rest functionality
+ */
+
 @RestController
 public class CarController {
-	@Autowired
-	private CarRepository carRepo;
+	private CarRepository repo;
 	
 	@RequestMapping("/cars")
-	public Iterable<Car> getCars() {
-		return carRepo.findAll();
-	}
-
+	public Iterable<Car> getCars() {return repo.findAll();}
 }
